90ac7e6da665d47184a7ef8474d05cc70387346c
Somber <dj2548@gmail.com>
2022-10-14 09:34:32 -0400

    circle circle

---
+++ circle objects/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace circle_objects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a radius: ");
+            Circle c1 = new Circle(5);
+            Console.WriteLine(c1.CalculateCir()); 
+
+        }
+  
+   
+    
+    }
+
+
+}0
---
+++ circle objects/circle .cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace circle_objects
+{
+    public class Circle
+    { // fields properties
+        public double Radius { get; set;}
+       //constructor
+        public Circle(double Radius) 
+        {
+            this.Radius = Radius;
+        }
+        //methods, access modifier, (static), return type, (method name), (parameters),
+        public string CalculateCir()
+        {
+            double circ = Radius * 2 * Math.PI;
+            return Math.Round(circ,2).ToString();
+
+        }
+          public string CalArea()
+        {
+            double area = Radius * Radius * Math.PI;
+            return Math.Round(area,2).ToString();
+        }
+        
+    }
+}
